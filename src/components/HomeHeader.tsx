b59c9d5c46d077cab1ab4af565f1ba0e4b2d0dbf
Mohamed El Mahallawy <mmahalwy@gmail.com>
2020-03-15 18:51:57 -0400

    init

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import headerImage from '../images/harvard-thermometer1.png';
+
+const HomeHeader = () => (
+  <div>
+    <img src={headerImage} />
+  </div>
+);
+
+export default HomeHeader;
